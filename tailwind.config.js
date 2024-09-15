6f29726060ac33c47c7b4755c24f9294555d50b5
Chudiniiii <matichudy205@gmail.com>
2024-09-15 19:11:53 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['C:/Users/matic/OneDrive/Pulpit/Inne/Rekrutacja/Rekrutacja.{html,js}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        bebas: ['Bebas Neue', 'sans-serif'],
+        pinksans: ['PinkSans', 'sans-serif'],
+      },
+      colors: {
+        customBlue: 'rgb(1, 70, 255)',
+        customGray: 'rgb(40, 40, 40)',
+      }
+    },
+  },
+  plugins: [],
+}
